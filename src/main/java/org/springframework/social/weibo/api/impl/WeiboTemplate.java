4ed375453fb6c8a8b8d81c55b0bb41fcf5776927
Denis Vergnes <denis.vergnes@gmail.com>
2011-12-21 14:34:25 +0800

    Update copyright

--- src/main/java/org/springframework/social/weibo/api/impl/WeiboTemplate.java
+++ src/main/java/org/springframework/social/weibo/api/impl/WeiboTemplate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 France Télécom
+ * Copyright 2011 France Telecom R&D Beijing Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
