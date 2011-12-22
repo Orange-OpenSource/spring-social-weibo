13cb3aa2fd0c65b55e6c1c6cf5c547e72291e858
Denis Vergnes <denis.vergnes@gmail.com>
2011-12-22 15:50:54 +0800

    Remove code in comment

--- src/main/java/org/springframework/social/weibo/connect/WeiboOAuth2Template.java
+++ src/main/java/org/springframework/social/weibo/connect/WeiboOAuth2Template.java
@@ -1,18 +1,18 @@
 /*
-* Copyright 2011 France Telecom R&D Beijing Co., Ltd 北京法国电信研发中心有限公司
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2011 France Telecom R&D Beijing Co., Ltd 北京法国电信研发中心有限公司
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.social.weibo.connect;
 
 import java.io.IOException;
@@ -77,18 +77,6 @@ public class WeiboOAuth2Template extends OAuth2Template {
 				return result;
 			}
 		};
-
-		// new MappingJacksonHttpMessageConverter() {
-		// public boolean canRead(Class<?> clazz, MediaType mediaType) {
-		// // always read as x-www-url-formencoded even though Facebook //
-		// // sets contentType to text/plain
-		// return true;
-		// }
-		// };
-		// List<MediaType> supportedMediaTypes = new ArrayList<MediaType>(2);
-		// supportedMediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
-		// supportedMediaTypes.add(MediaType.MULTIPART_FORM_DATA);
-		// messageConverter.setSupportedMediaTypes(supportedMediaTypes);
 
 		restTemplate.setMessageConverters(Collections
 				.<HttpMessageConverter<?>> singletonList(messageConverter));
