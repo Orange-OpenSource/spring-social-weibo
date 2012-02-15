90249a429595a6479134ec29cd6bdbe72f4f6fad
Denis Vergnes <denis.vergnes@orange.com>
2012-02-15 08:40:47 +0800

    Remove commented code

--- src/main/java/org/springframework/social/weibo/api/impl/AccountTemplate.java
+++ src/main/java/org/springframework/social/weibo/api/impl/AccountTemplate.java
@@ -1,18 +1,18 @@
 /*
-* Copyright 2011 France Telecom R&D Beijing Co., Ltd 北京法国电信研发中心有限公司
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2011 France Telecom R&D Beijing Co., Ltd 北京法国电信研发中心有限公司
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.social.weibo.api.impl;
 
 import java.util.Map;
@@ -32,15 +32,6 @@ class AccountTemplate extends AbstractWeiboOperations implements
 	@Override
 	public String getUid() {
 		requireAuthorization();
-		// return restTemplate.execute(buildUri("account/get_uid.json"),
-		// HttpMethod.GET, null, new ResponseExtractor<String>() {
-		//
-		// @Override
-		// public String extractData(ClientHttpResponse response)
-		// throws IOException {
-		// return IOUtils.toString(response.getBody());
-		// }
-		// });
 		return restTemplate
 				.getForObject(buildUri("account/get_uid.json"), Map.class)
 				.get("uid").toString();
