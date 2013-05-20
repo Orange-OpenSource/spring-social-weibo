fc21c999eb94f5cdc0234e2c4f7a89b4d949cf27
arkxu <fangzhou@arkxu.com>
2013-05-19 19:40:22 -0700

    weibo return expires_in in millisecond

--- src/main/java/org/springframework/social/weibo/connect/WeiboOAuth2Template.java
+++ src/main/java/org/springframework/social/weibo/connect/WeiboOAuth2Template.java
@@ -39,65 +39,67 @@ import org.springframework.web.client.RestTemplate;
 
 public class WeiboOAuth2Template extends OAuth2Template {
 
-	private static final Log logger = LogFactory
-			.getLog(WeiboOAuth2Template.class.getName());
+    private static final Log logger = LogFactory
+            .getLog(WeiboOAuth2Template.class.getName());
 
-	public WeiboOAuth2Template(String clientId, String clientSecret) {
-		super(clientId, clientSecret,
-				"https://api.t.sina.com.cn/oauth2/authorize",
-				"https://api.t.sina.com.cn/oauth2/access_token");
-	}
+    public WeiboOAuth2Template(String clientId, String clientSecret) {
+        super(clientId, clientSecret,
+                "https://api.t.sina.com.cn/oauth2/authorize",
+                "https://api.t.sina.com.cn/oauth2/access_token");
+    }
 
-	@Override
-	protected RestTemplate createRestTemplate() {
-		RestTemplate restTemplate = new RestTemplate(
-				ClientHttpRequestFactorySelector.getRequestFactory());
-		HttpMessageConverter<?> messageConverter = new FormHttpMessageConverter() {
+    @Override
+    protected RestTemplate createRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate(
+                ClientHttpRequestFactorySelector.getRequestFactory());
+        HttpMessageConverter<?> messageConverter = new FormHttpMessageConverter() {
 
-			private final ObjectMapper objectMapper = new ObjectMapper();
+            private final ObjectMapper objectMapper = new ObjectMapper();
 
-			@Override
-			public boolean canRead(Class<?> clazz, MediaType mediaType) {
-				return true;
-			}
+            @Override
+            public boolean canRead(Class<?> clazz, MediaType mediaType) {
+                return true;
+            }
 
-			@Override
-			public MultiValueMap<String, String> read(
-					Class<? extends MultiValueMap<String, ?>> clazz,
-					HttpInputMessage inputMessage) throws IOException,
-					HttpMessageNotReadableException {
+            @Override
+            public MultiValueMap<String, String> read(
+                    Class<? extends MultiValueMap<String, ?>> clazz,
+                    HttpInputMessage inputMessage) throws IOException,
+                    HttpMessageNotReadableException {
 
-				TypeReference<Map<String, ?>> mapType = new TypeReference<Map<String, ?>>() {
-				};
-				LinkedHashMap<String, ?> readValue = objectMapper.readValue(
-						inputMessage.getBody(), mapType);
-				LinkedMultiValueMap<String, String> result = new LinkedMultiValueMap<String, String>();
-				for (Entry<String, ?> currentEntry : readValue.entrySet()) {
-					result.add(currentEntry.getKey(), currentEntry.getValue()
-							.toString());
-				}
-				return result;
-			}
-		};
+                TypeReference<Map<String, ?>> mapType = new TypeReference<Map<String, ?>>() {
+                };
+                LinkedHashMap<String, ?> readValue = objectMapper.readValue(
+                        inputMessage.getBody(), mapType);
+                LinkedMultiValueMap<String, String> result = new LinkedMultiValueMap<String, String>();
+                for (Entry<String, ?> currentEntry : readValue.entrySet()) {
+                    result.add(currentEntry.getKey(), currentEntry.getValue()
+                            .toString());
+                }
+                return result;
+            }
+        };
 
-		restTemplate.setMessageConverters(Collections
-				.<HttpMessageConverter<?>> singletonList(messageConverter));
-		return restTemplate;
-	}
+        restTemplate.setMessageConverters(Collections
+                .<HttpMessageConverter<?>> singletonList(messageConverter));
+        return restTemplate;
+    }
 
-	@Override
-	@SuppressWarnings("unchecked")
-	protected AccessGrant postForAccessGrant(String accessTokenUrl,
-			MultiValueMap<String, String> parameters) {
-		MultiValueMap<String, String> response = getRestTemplate()
-				.postForObject(accessTokenUrl, parameters, MultiValueMap.class);
-		String expires = response.getFirst("expires_in");
-		String accessToken = response.getFirst("access_token");
-		if (logger.isDebugEnabled()) {
-			logger.debug("access token value = " + accessToken);
-		}
-		return new AccessGrant(accessToken, null, null,
-				expires != null ? Integer.valueOf(expires) : null);
-	}
+    @Override
+    @SuppressWarnings("unchecked")
+    protected AccessGrant postForAccessGrant(String accessTokenUrl,
+            MultiValueMap<String, String> parameters) {
+        MultiValueMap<String, String> response = getRestTemplate()
+                .postForObject(accessTokenUrl, parameters, MultiValueMap.class);
+        String expires = response.getFirst("expires_in");
+        String accessToken = response.getFirst("access_token");
+        if (logger.isDebugEnabled()) {
+            logger.debug("access token value = " + accessToken);
+        }
+
+        Integer expiresInSecond = expires != null ? Integer.valueOf(expires) / 1000
+                : null;
+        return new AccessGrant(accessToken, null, null, expiresInSecond);
+    }
 
 }
